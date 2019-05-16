22a5165a108b38dbfeb616b62a996efbb0460bcb
Sally Dabbah <sallydabbah@gmail.com>
2019-05-16 12:14:24 +0300

    Update postComponent.vue
    selected post is viewed using store data

--- src/components/postComponent.vue
+++ src/components/postComponent.vue
@@ -17,16 +17,17 @@ import 'bootstrap-vue/dist/bootstrap-vue.css';
 export default{
   data(){
     return {
-       id:this.$route.params.id,
-       post:{}
+       id:this.$route.params.id
     }
   },
   created(){
-    this.$http.get('https://jsonplaceholder.typicode.com/posts/' + this.id).then(function(data){
-        console.log(data);
-         this.post=data.body;
-    })
-  }
+                this.$store.dispatch('loadSelectedPost','https://jsonplaceholder.typicode.com/posts/' + this.id);
+  },
+  computed:{
+      post(){
+        return this.$store.state.selectedPost;
+      }
+    }
 }
 </script>
 
